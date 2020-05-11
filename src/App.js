6df7660d53228fc734555e58e599720b3e4d93a0
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2020-05-11 19:05:22 +0900

    Add eslint, editorconfig and env file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import VisualizeDiagramForceSimulation from "./VisualizeDiagramForceSimulation"
+import React from 'react'
+import VisualizeDiagramForceSimulation from './VisualizeDiagramForceSimulation'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <VisualizeDiagramForceSimulation modelFile="target3b.json" visualizer="forceSimulation" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
